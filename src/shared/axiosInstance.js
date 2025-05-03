dd88a8c67a0ea425d7c0406b121a0a0a616215cf
Hammad Zubair <hammadzubairofficial@gmail.com>
2025-05-03 21:35:17 +0500

    Update the client base url

--- src/shared/axiosInstance.js
+++ src/shared/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: '/api',
+    baseURL: 'https://foodie-fiesta-backend1.vercel.app/api',
 });
 
 // Add token to every request automatically
